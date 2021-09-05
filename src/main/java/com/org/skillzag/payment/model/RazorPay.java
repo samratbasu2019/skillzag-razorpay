b229a463adcddc4b7affe4f2038e4aa8e0e0485f
samrat <samtina.basu@gmail.com>
2021-09-05 17:22:31 +0530

    updated

--- src/main/java/com/org/skillzag/payment/model/RazorPay.java
+++ src/main/java/com/org/skillzag/payment/model/RazorPay.java
@@ -1,5 +1,7 @@
 package com.org.skillzag.payment.model;
 
+import lombok.Data;
+
 /**
  * 
  * 
@@ -8,6 +10,7 @@ package com.org.skillzag.payment.model;
  *
  */
 
+@Data
 public class RazorPay {
 
 	
@@ -38,108 +41,8 @@ public class RazorPay {
 	public RazorPay() {
 
 	}
-
 	public RazorPay(String applicationFee) {
 		this.applicationFee = applicationFee;
 	}
-
-	public String getApplicationFee() {
-		return applicationFee;
-	}
-
-	public void setApplicationFee(String applicationFee) {
-		this.applicationFee = applicationFee;
-	}
-
-	public String getRazorpayOrderId() {
-		return razorpayOrderId;
-	}
-
-	public void setRazorpayOrderId(String razorpayOrderId) {
-		this.razorpayOrderId = razorpayOrderId;
-	}
-
-	public String getSecretKey() {
-		return secretKey;
-	}
-
-	public void setSecretKey(String secretKey) {
-		this.secretKey = secretKey;
-	}
-
-	public String getMerchantName() {
-		return merchantName;
-	}
-
-	public void setMerchantName(String merchantName) {
-		this.merchantName = merchantName;
-	}
-
-	public String getPurchaseDescription() {
-		return purchaseDescription;
-	}
-
-	public void setPurchaseDescription(String purchaseDescription) {
-		this.purchaseDescription = purchaseDescription;
-	}
-
-	public String getCustomerName() {
-		return customerName;
-	}
-
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
-
-	public String getCustomerEmail() {
-		return customerEmail;
-	}
-
-	public void setCustomerEmail(String customerEmail) {
-		this.customerEmail = customerEmail;
-	}
-
-	public String getCustomerContact() {
-		return customerContact;
-	}
-
-	public void setCustomerContact(String customerContact) {
-		this.customerContact = customerContact;
-	}
-
-	public String getPaymentId() {
-		return paymentId;
-	}
-
-	public void setPaymentId(String paymentId) {
-		this.paymentId = paymentId;
-	}
-
-	public String getNotes() {
-		return notes;
-	}
-
-	public void setNotes(String notes) {
-		this.notes = notes;
-	}
-
-	public String getImageURL() {
-		return imageURL;
-	}
-
-	public void setImageURL(String imageURL) {
-		this.imageURL = imageURL;
-	}
-
-	public String getTheme() {
-		return theme;
-	}
-
-	public void setTheme(String theme) {
-		this.theme = theme;
-	}
-
-	
-
 
 }
--- src/main/java/com/org/skillzag/payment/model/Response.java
+++ src/main/java/com/org/skillzag/payment/model/Response.java
@@ -1,29 +1,15 @@
 package com.org.skillzag.payment.model;
 
+import lombok.Data;
+
 /**
  * 
  * @author rahul
  *
  */
+@Data
 public class Response {
-	
 	private int statusCode;
 	private RazorPay razorPay;
 
-	public int getStatusCode() {
-		return statusCode;
-	}
-	public void setStatusCode(int statusCode) {
-		this.statusCode = statusCode;
-	}
-	public RazorPay getRazorPay() {
-		return razorPay;
-	}
-
-	public void setRazorPay(RazorPay razorPay) {
-		this.razorPay = razorPay;
-	}
-
-	
-	
 }
